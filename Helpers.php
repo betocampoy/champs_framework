5748ecb8f1d7d989e43559ff99f16a2773a7794a
Beto Campoy <beto.campoy@gmail.com>
2022-03-12 18:53:44 -0300

    composer.json updates

--- Helpers.php
+++ Helpers.php
@@ -1,1 +1,14 @@
 <?php
+
+
+if(!function_exists("session")) {
+    /**
+     * Returns an instance of session
+     *
+     * @return \BetoCampoy\ChampsSao\Session
+     */
+    function session(): \BetoCampoy\ChampsSao\Session
+    {
+        return new \BetoCampoy\ChampsSao\Session();
+    }
+}
