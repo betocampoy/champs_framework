9ae5bacd4e865aae423116160e5c256e95c01520
Beto Campoy <beto.campoy@gmail.com>
2022-05-05 17:49:51 -0300

    Tests

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,43 +5,57 @@ namespace BetoCampoy\ChampsFramework\Controller;
 
 
 use BetoCampoy\ChampsFramework\Controller\Contracts\AuthContract;
+use BetoCampoy\ChampsFramework\Models\Auth\User;
 
 class AuthController extends Controller implements AuthContract
 {
-
-    public function loginForm(?array $data): void
-    {
-        var_dump("teste");
-    }
+    protected bool $protectedController = false;
 
     public function loginExecute(?array $data): void
     {
-        // TODO: Implement loginExecute() method.
+        if (request_limit("weblogin", 3, 60 * 5)) {
+            $json['message'] = $this->message
+              ->error("Você já efetuou 3 tentativas, esse é o limite. Por favor, aguarde 5 minutos para tentar novamente!")
+              ->render();
+            echo json_encode($json);
+            return;
+        }
+
+        if (empty($data['email']) || empty($data['password'])) {
+            $json['message'] = $this->message
+              ->warning("Informe seu email e senha para entrar")
+              ->render();
+            echo json_encode($json);
+            return;
+        }
+
+        $save = (!empty($data['save']) ? true : false);
+        $auth = new User();
+        $login = $auth->login($data['email'], $data['password'], $save);
+
+        if ($login) {
+            $this->message
+              ->success("Seja bem-vindo(a) de volta " . user()->name . "!")
+              ->flash();
+            $this->redirect($this->router->route("dash"));
+        }
+        else {
+            $json['message'] = $auth->message()->before("Ooops! ")->render();
+        }
+
+        echo json_encode($json);
+        return;
     }
 
-    public function registerForm(?array $data): void
-    {
-        // TODO: Implement registerForm() method.
-    }
 
     public function registerExecute(?array $data): void
     {
         // TODO: Implement registerExecute() method.
     }
 
-    public function forgetForm(?array $data): void
-    {
-        // TODO: Implement forgetForm() method.
-    }
-
     public function forgetExecute(?array $data): void
     {
         // TODO: Implement forgetExecute() method.
-    }
-
-    public function resetForm(?array $data): void
-    {
-        // TODO: Implement resetForm() method.
     }
 
     public function resetExecute(?array $data): void
