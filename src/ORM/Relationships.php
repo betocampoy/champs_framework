0d39bac671b5faf490dcacf30484fcd93be0d912
betocampoy <beto.campoy@gmail.com>
2023-02-15 13:16:09 -0300

    Framework help

--- src/ORM/Relationships.php
+++ src/ORM/Relationships.php
@@ -92,7 +92,6 @@ trait Relationships
         }
 
         $model = (new $related_model_class)->findById($this->$foreign_key);
-        var_dump($model);die();
         return empty($model) ? null : $model;
     }
 
